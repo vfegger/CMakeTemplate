fb3e05683047d9a52d411763b996624384546b1c
Vitor Fernandes Egger <vitor@psr-inc.com>
2022-12-23 00:19:31 -0300

    Add template files

---
+++ src/template/templateSource.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+// This input is generated dynamically
+#include "templateConfig.h"
+
+int main(){
+    std::cout << "Version: "<< Template_VERSION_MAJOR << "." << Template_VERSION_MINOR << std::endl;
+}
